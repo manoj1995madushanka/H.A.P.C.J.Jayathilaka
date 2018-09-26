1ccd89f66f11861bea1d7879b93c441df950cdb0
chamodi08jaya <mailtochamodij@gmail.com>
2018-09-27 01:14:40 +0530

    1st attempt

---
+++ TAK TIK TUK/src/tak/tik/tuk/TAKTIKTUK.java
@@ -0,0 +1,19 @@
+
+package tak.tik.tuk;
+
+/**
+ *
+ * @author Chamodi
+ */
+public class TAKTIKTUK {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainFrame mf = new MainFrame();
+        mf.setVisible(true);
+        
+        // TODO code application logic here
+    }
+}
